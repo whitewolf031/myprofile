1720051eea641ab956b74017469cd42227400af6
Sardorbek031 <sardorbekergashev031@gmail.com>
2025-09-21 16:02:37 +0500

    change http

--- myprofile/serializers.py
+++ myprofile/serializers.py
@@ -1,27 +1,25 @@
 from rest_framework import serializers
-from .models import models, admin_models
-
+from .models.models import *
+from .models.admin_models import *
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.UsersInfo
+        model = UsersInfo
         fields = ['name', 'email', 'message']
-
-
 
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
-        model = admin_models.DevInfo
+        model = DevInfo
         fields = "__all__"
 
 
 class AdminExperienceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = admin_models.Experience
+        model = Experience
         fields = "__all__"
 
 
 class AdminProjectSerializer(serializers.ModelSerializer):
     class Meta:
-        model = admin_models.Project
+        model = Project
         fields = "__all__"
