5c705090e10a10cf25087f89a622fbbdf7c4b73f
Sardorbek031 <sardorbekergashev031@gmail.com>
2025-09-23 17:37:14 +0500

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-# Migrate qilish
 python manage.py migrate --noinput
 
-# Agar superuser mavjud bo‘lmasa — yaratib qo‘yish
-python manage.py createsuperuser \
-  --noinput \
-  --username admin \
-  --email admin@example.com || true \
-  --password admin1234
+# Superuser mavjudligini tekshir
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+import os; \
+username=os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin'); \
+email=os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com'); \
+password=os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123'); \
+\
+if not User.objects.filter(username=username).exists(): \
+    User.objects.create_superuser(username=username, email=email, password=password)" \
+    | python manage.py shell
 
 # Django serverni ishga tushirish
 exec "$@"
