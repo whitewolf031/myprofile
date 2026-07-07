5a6115ad2f29a03d6a3330432d641c165d5478d1
Sardorbek031 <w72075433@gmail.com>
2026-07-07 16:51:09 +0500

    change deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,30 +20,29 @@ jobs:
 
       - name: Add server to known hosts
         run: |
-          ssh-keyscan -p ${{ secrets.SSH_PORT }} -H ${{ secrets.SSH_HOST }} >> ~/.ssh/known_hosts
+          ssh-keyscan -H ${{ secrets.SSH_HOST }} >> ~/.ssh/known_hosts
 
       - name: Deploy to server
         run: |
-          ssh -p ${{ secrets.SSH_PORT }} \
-            ${{ secrets.SSH_USER }}@${{ secrets.SSH_HOST }} << 'EOF'
+          ssh ${{ secrets.SSH_USER }}@${{ secrets.SSH_HOST }} << 'EOF'
 
-          cd /var/www/myprofile
+          cd /projects/myprofile
 
           echo "Pull latest code..."
           git pull origin main
 
           echo "Stopping containers..."
-          docker-compose down
+          docker compose down
 
-          echo "Building and starting..."
-          docker-compose up -d --build
+          echo "Building containers..."
+          docker compose up -d --build
 
-          echo "Migration running..."
-          docker-compose exec -T backend python manage.py migrate
+          echo "Running migrations..."
+          docker compose exec -T backend python manage.py migrate
 
-          echo "Collect static..."
-          docker-compose exec -T backend python manage.py collectstatic --noinput
+          echo "Collecting static files..."
+          docker compose exec -T backend python manage.py collectstatic --noinput
 
-          echo "Done 🚀"
+          echo "Deployment completed successfully 🚀"
 
           EOF
